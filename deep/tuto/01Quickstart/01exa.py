661ded767d7bed21029ce5214364df413ecbcccc
nakmi <1770379469@qq.com>
2026-04-02 18:22:41 +0800

    add: langchain

---
+++ deep/tuto/01Quickstart/01exa.py
@@ -0,0 +1,34 @@
+from deepagents import create_deep_agent
+from langchain.messages import HumanMessage
+from tools import internet_search
+from llm import get_llm
+from rich import print
+from langfuse_tracing import langfuse_handler
+# System prompt to steer the agent to be an expert researcher
+research_instructions = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+
+You have access to an internet search tool as your primary means of gathering information.
+
+## `internet_search`
+
+Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+"""
+
+model = get_llm("dashscope", "qwen3.5-flash")
+
+agent = create_deep_agent(
+    model=model,
+    system_prompt=research_instructions,
+    tools=[internet_search],
+).with_config(
+    {
+        "callbacks": [langfuse_handler]
+    }
+)
+
+response = agent.invoke(
+    {
+        "messages": [HumanMessage(content="今天成都的天气？")]
+    }
+)
+print(response)
---
+++ langfuse_tracing.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langfuse import get_client
+from langfuse.langchain import CallbackHandler
+
+# Initialize Langfuse client
+langfuse = get_client()
+
+# Initialize Langfuse CallbackHandler for Langchain (tracing)
+langfuse_handler = CallbackHandler()
